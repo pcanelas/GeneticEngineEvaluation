4e8289976b1f52229a5617cf60e34449f098c339
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2021-11-23 15:00:28 +0000

    Updated setup with Alcides PonyGE

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone git@github.com:alcides/GeneticEngine.git
-git clone https://github.com/PonyGE/PonyGE2.git
+git clone git@github.com:alcides/PonyGE2.git
 cd GeneticEngine 
 python3 -m pip install -r requirements.txt
 cd ../PonyGE2
--- src/plot_data.py
+++ src/plot_data.py
@@ -5,47 +5,43 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def boxplot_df(ponyge_df, gengine_df, example, description):
+    ponyge_vals = ponyge_df[description].values
+    gengine_vals = gengine_df[description].values
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.violinplot([np.log10(ponyge_vals), np.log10(gengine_vals)])
+    tix = ['PonyGE', 'GeneticEngine']
+    plt.xticks([1,2], tix, rotation=0, fontsize=12)
+    plt.title(f"{description.capitalize()} time per Tool")
+    ax.set_ylabel("Time log(ns)", fontsize=12)
+    ax.set_xlabel('Tools', fontsize=12)
+
+    plt.savefig(f"plots/{example}_{description}.pdf")
+    plt.close()
+
+
 if __name__ == '__main__':
     
     examples = sys.argv[1:]
 
     ponyge_times = helper.import_data(examples, 'results/ponyge/', ',')
     gengine_times = helper.import_data(examples, 'results/gengine/', ',')
-    print(examples)
+    
+    helper.create_folder("plots/")
+
     for example in examples:
-        print(example)
+    
         if not example in ponyge_times or example in gengine_times:
             pass
         
         ponyge_df = ponyge_times[example]
         gengine_df = gengine_times[example]
 
-        tp1 = ponyge_df['processing_time'].values
-        tp2 = gengine_df['processing_time'].values
+        ponyge_df['total'] = ponyge_df['processing_time'] + ponyge_df['evolution_time']
+        gengine_df['total'] = gengine_df['processing_time'] + gengine_df['evolution_time']
 
-        et1 = ponyge_df['evolution_time'].values
-        et2 = gengine_df['evolution_time'].values
-
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        ax.boxplot([tp1, tp2])
-        tix = ['PonyGE', 'GeneticEngine']
-        plt.xticks([1,2], tix, rotation=0, fontsize=12)
-        plt.title("Processing time per Tool")
-        ax.set_ylabel("Time (ns)", fontsize=12)
-        ax.set_xlabel('Tools', fontsize=12)
-
-        plt.savefig(f"{example}_processing.pdf")
-        plt.close()
-
-
-        fig2 = plt.figure()
-        ax = fig2.add_subplot(1, 1, 1)
-        ax.boxplot([et1, et2])
-        tix = ['PonyGE', 'GeneticEngine']
-        plt.xticks([1,2], tix, rotation=0, fontsize=12)
-        plt.title("Evolution time per Tool")
-        ax.set_ylabel("Time (ns)", fontsize=12)
-        ax.set_xlabel('Tools', fontsize=12)
-
-        plt.savefig(f"{example}_evolution.pdf")
+        boxplot_df(ponyge_df, gengine_df, example, 'processing_time')
+        boxplot_df(ponyge_df, gengine_df, example, 'evolution_time')
+        boxplot_df(ponyge_df, gengine_df, example, 'total')
