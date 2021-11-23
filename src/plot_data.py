7ea42d90002228ba53ab86d12d64eef3ef2911a3
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2021-11-23 15:12:36 +0000

    Updated plotting with feedback

--- src/plot_data.py
+++ src/plot_data.py
@@ -1,23 +1,25 @@
 import sys
 import src.evaluation.helper as helper
 
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def boxplot_df(ponyge_df, gengine_df, example, description):
+def violinplot_df(ponyge_df, gengine_df, example, description):
     ponyge_vals = ponyge_df[description].values
     gengine_vals = gengine_df[description].values
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.violinplot([np.log10(ponyge_vals), np.log10(gengine_vals)])
+    ax.violinplot([ponyge_vals, gengine_vals])
     tix = ['PonyGE', 'GeneticEngine']
     plt.xticks([1,2], tix, rotation=0, fontsize=12)
     plt.title(f"{description.capitalize()} time per Tool")
-    ax.set_ylabel("Time log(ns)", fontsize=12)
+    ax.set_ylabel("Time (s)", fontsize=12)
     ax.set_xlabel('Tools', fontsize=12)
 
+    ax.set_ylim(ymin=0)
     plt.savefig(f"plots/{example}_{description}.pdf")
     plt.close()
 
@@ -39,9 +41,15 @@ if __name__ == '__main__':
         ponyge_df = ponyge_times[example]
         gengine_df = gengine_times[example]
 
+        cols = ['processing_time', 'evolution_time']
+
+        # Convert from ns to s
+        for df, column in itertools.product([ponyge_df, gengine_df], cols):
+            df[column] = df[column].apply(lambda x: x * pow(10, -9))
+        
         ponyge_df['total'] = ponyge_df['processing_time'] + ponyge_df['evolution_time']
         gengine_df['total'] = gengine_df['processing_time'] + gengine_df['evolution_time']
 
-        boxplot_df(ponyge_df, gengine_df, example, 'processing_time')
-        boxplot_df(ponyge_df, gengine_df, example, 'evolution_time')
-        boxplot_df(ponyge_df, gengine_df, example, 'total')
+        violinplot_df(ponyge_df, gengine_df, example, 'processing_time')
+        violinplot_df(ponyge_df, gengine_df, example, 'evolution_time')
+        violinplot_df(ponyge_df, gengine_df, example, 'total')
