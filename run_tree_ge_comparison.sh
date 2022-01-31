c12c595afeaaa2d75291d381cc384b054bcc6a57
Leon Ingelse <leoningelse@gmail.com>
2022-01-31 18:53:17 +0000

    Create run_tree_ge_comparison.sh

---
+++ run_tree_ge_comparison.sh
@@ -0,0 +1,10 @@
+export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}./GeneticEngine"
+#!/bin/bash
+#version=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+
+#if [ "$version" -lt "39" ]; then
+#    echo "python >= 3.9 is required to run evaluations."
+#    exit 1
+#fi
+
+python3.9 -m src.treebased_ge_comparison $*
