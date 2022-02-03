bac9f762bdeae235b1c3a01565a8333d91d74acc
PedroBarbosa <psbpedrobarbosa@gmail.com>
2022-02-03 14:39:41 +0000

    Starting to work on production ready plots

--- src/ponyge_comparison/visualization/plot_data_together.py
+++ src/ponyge_comparison/visualization/plot_data_together.py
@@ -9,26 +9,85 @@ plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 import seaborn as sns
 
-def plot_df(merged_dataframe, plot_info):
+# def plot_df(merged_dataframe, plot_info):
     
-    ax = sns.boxplot(data=merged_dataframe)
+#     ax = sns.boxplot(data=merged_dataframe)
     
-    tix = ['classification', 'game_of_life', 'median', 'number_io', 'regression', 'smallest', 'string_match', 'sum_of_squares', 'vectorial']
-    #tix = "abcdefghi"
-    plt.xticks([0.5, 2.5, 4.5, 6.5, 8.5, 10.5, 12.5, 14.5, 16.5], tix, rotation=45, fontsize=8)
-    plt.title("Titulo temporario")
+#     tix = ['classification', 'game_of_life', 'median', 'number_io', 'regression', 'smallest', 'string_match', 'sum_of_squares', 'vectorial']
+#     #tix = "abcdefghi"
+#     plt.xticks([0.5, 2.5, 4.5, 6.5, 8.5, 10.5, 12.5, 14.5, 16.5], tix, rotation=45, fontsize=8)
+#     plt.title("Titulo temporario")
     
-    ax.set_ylabel("Relative performance", fontsize=12)
-    #ax.set_xlabel('Tools', fontsize=12)
+#     ax.set_ylabel("Relative performance", fontsize=12)
+#     #ax.set_xlabel('Tools', fontsize=12)
 
-    ax.set_ylim(ymin=0)
+#     ax.set_ylim(ymin=0)
     
-    plt.axvline(1.5, ls="--")
+#     plt.axvline(1.5, ls="--")
+#     plt.tight_layout()
+#     plt.savefig(f"plots/merged_plots.pdf")
+#     plt.close()
+
+
+def draw_barplot(df: pd.DataFrame, outbasename: str, what_to_plot: str = "Fitness"):
+    """
+    Draws a barplot that compares the two tools 
+    relatively to some metric passed in the 'what_to_plot' 
+    argument, for all the existing examples.
+    """
+    
+    df['Relative {}'.format(what_to_plot)] = df.apply(lambda x: x[what_to_plot] / 
+                                                      df[df.Tool.str.contains('PonyGE2') & df.Benchmark.str.contains(x.Benchmark)].mean(), axis=1)
+
+    axis = sns.barplot(data=df, 
+                       x='Benchmark', 
+                       y='Relative {}'.format(what_to_plot), 
+                       hue='Tool')
+
+    for item in axis.get_xticklabels():
+        item.set_rotation(25)
+
+    #plt.title("Relative Fitness of PonyGE2 and GeneticEngine")
     plt.tight_layout()
-    plt.savefig(f"plots/merged_plots.pdf")
+    plt.savefig("plots/{}.pdf".format(outbasename))
     plt.close()
+    
+def draw_violin_with_facets(df:pd.DataFrame, outbasename: str, what_to_plot: str = "Fitness"):
+    """
+    Draws violin plots for all examples.
+    
+    Each facet represents an example, which
+    allows for the scale of the 'what_to_plot' to reflect
+    absolute values, rather than relative.
+    """
+    melt_data = data.melt(id_vars=['max_depth', 'tool'], var_name='Benchmark', value_name='Fitness')
 
+    to_replace = {'classification': 'classification (higher_is_better)',
+                'game_of_life': 'game_of_life (higher_is_better)',
+                'regression': 'regression (lower_is_better)',
+                'string_match': 'string_match (lower_is_better)',
+                'vectorialgp': 'vectorialgp (lower_is_better)'}
 
+    melt_data = melt_data.replace(to_replace)
+
+    palette ={"PonyGE2": "steelblue", "GeneticEngine": "pink"}
+    fig = plt.figure(figsize=(8,6))
+    g = sns.catplot(x='max_depth', y='Fitness', hue='tool',
+                    sharey=False,
+                    sharex=False,
+                    palette=palette,
+                    height=3, 
+                    aspect=1,
+                    kind='violin',
+                    col='Benchmark',
+                    col_wrap=3,
+                    data=melt_data)
+
+    (g.set_axis_labels("Max tree depth", "Fitness")
+    .set_titles("{col_name}")
+    .despine(left=True))  
+
+    g.savefig('test.pdf')
 
 
 
@@ -75,7 +134,7 @@ if __name__ == '__main__':
         
         
         # #########################################################################
-        # Create the Merged Plot of time
+        # Create the Merged Plot of time taken to do the evolution
         cols = ['Tool', 'Benchmark', 'Time']
         rows = []
 
@@ -96,26 +155,11 @@ if __name__ == '__main__':
                 rows.append(['GEngine', example, val])
             
         merged_dataframe =  pd.DataFrame(data=rows, columns=cols)
-        
-        # Calculate the average
-        # print(merged_dataframe)
-
-        merged_dataframe['Relative Time'] = merged_dataframe.apply(lambda x: x['Time'] / merged_dataframe[merged_dataframe['Tool'].str.contains('PonyGE2') & merged_dataframe['Benchmark'].str.contains(x['Benchmark'])].mean(), axis=1)
-
-        axis = sns.barplot(data=merged_dataframe, x='Benchmark', y='Relative Time', hue='Tool')
-
-        for item in axis.get_xticklabels():
-            item.set_rotation(25)
-
-        plt.title("Relative Time of PonyGE2 and GeneticEngine")
-        plt.tight_layout()
-        plt.savefig(f"plots/merged_plots_time.pdf")
-        plt.close()
-
-
+        draw_barplot(merged_dataframe, outbasename='merged_plots_time', what_to_plot='Time')
+       
 
     # #########################################################################
-    # Create the Merged Plot of Fitness
+    # Create the Merged Plot of Fitness in timer mode
     if len(ponyge_timer) == len(gengy_timer) != 0:
         print("Running fitness within time limit plot")
 
@@ -140,17 +184,7 @@ if __name__ == '__main__':
             
         merged_dataframe =  pd.DataFrame(data=rows, columns=cols)
         
-        # Calculate the average
-        # print(merged_dataframe)
+        draw_barplot(merged_dataframe, outbasename='merged_plots_fitness', what_to_plot='Fitness')
 
-        merged_dataframe['Relative Fitness'] = merged_dataframe.apply(lambda x: x['Fitness'] / merged_dataframe[merged_dataframe['Tool'].str.contains('PonyGE2') & merged_dataframe['Benchmark'].str.contains(x['Benchmark'])].mean(), axis=1)
-
-        axis = sns.barplot(data=merged_dataframe, x='Benchmark', y='Relative Fitness', hue='Tool')
-
-        for item in axis.get_xticklabels():
-            item.set_rotation(25)
-
-        plt.title("Relative Fitness of PonyGE2 and GeneticEngine")
-        plt.tight_layout()
-        plt.savefig(f"plots/merged_plots_fitness.pdf")
-        plt.close()
+       
+        plot
